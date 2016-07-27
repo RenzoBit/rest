76faa50840da0fe52671bb62b7691237a5b6527c
RenzoBit <tegobijava@gmail.com>
2016-07-27 12:34:49 -0500

    .

--- classes/common/Conexion.php
+++ classes/common/Conexion.php
@@ -1,18 +1,15 @@
 <?php
-
 class Conexion {
-
-	static private $instance;
-	
+	private static $instance;
 	public static function getConexion() {
-		if (self::$instance == NULL){           
-			$PDOinstance = new PDO('mysql:host=localhost;dbname=rvilela_android;charset=utf8', 'rvilela_android', '123abc');
-			$PDOinstance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		if (self::$instance == NULL) {
+			//$PDOinstance = new PDO ( 'mysql:host=localhost;dbname=rvilela_android;charset=utf8', 'rvilela_android', '123abc' );
+			$PDOinstance = new PDO ( 'mysql:host=stevie.heliohost.org;dbname=rvilela_android;charset=utf8', 'rvilela_android', '123abc' );
+			$PDOinstance->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 			self::$instance = $PDOinstance;
 		}
 		return self::$instance;
 	}
-	
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,135 +1,142 @@
 <?php
-
 require_once './autoload.php';
 require_once './lib/Slim/Slim.php';
 
 // Autocarga de la librería
-\Slim\Slim::registerAutoloader();
+\Slim\Slim::registerAutoloader ();
 
 // Creando una instancia del Slim
-$app = new \Slim\Slim();
-$app->response->header('Content-Type', 'application/json');
+$app = new \Slim\Slim ();
+$app->response->header ( 'Content-Type', 'application/json' );
 
-$app->get('/productos', function() {
-	$dao = new ProductoDAO();
-	$lista = $dao->listar();    
-	echo json_encode($lista);    
-});
+$app->get ( '/productos', function () {
+	$dao = new ProductoDAO ();
+	$lista = $dao->listar ();
+	echo json_encode ( $lista );
+} );
 
-$app->get('/productos/:nombre', function($nombre) {
-	$dao = new ProductoDAO(); 
-	$objeto = $dao->buscarPorNombre($nombre);    
-	echo json_encode($objeto);    
-});
+$app->get ( '/productos/:nombre', function ($nombre) {
+	$dao = new ProductoDAO ();
+	$objeto = $dao->buscarPorNombre ( $nombre );
+	echo json_encode ( $objeto );
+} );
 
-$app->post('/productos', function() use ($app) {
-	$idCategoria = $app->request()->post('idCategoria');
-	$nombre = $app->request()->post('nombre');
-	$precio = $app->request()->post('precio');
-
-	$vo = new Producto();
+$app->post ( '/productos', function () use ($app) {
+	$idCategoria = $app->request ()->post ( 'idCategoria' );
+	$nombre = $app->request ()->post ( 'nombre' );
+	$precio = $app->request ()->post ( 'precio' );
+	
+	$vo = new Producto ();
 	$vo->idCategoria = $idCategoria;
 	$vo->nombre = $nombre;
-	$vo->precio = $precio; 
-
-	$dao = new ProductoDAO();
-	$dao->insertar($vo);    
-	echo json_encode(array('mensaje' => 'Producto registrado satisfactoriamente'));
-});
+	$vo->precio = $precio;
+	
+	$dao = new ProductoDAO ();
+	$dao->insertar ( $vo );
+	echo json_encode ( array (
+			'mensaje' => 'Producto registrado satisfactoriamente' 
+	) );
+} );
 
 /*
-$app->put('/productos/:id', function($id) use ($app) {
-	$body = $app->request->getBody();
-	$producto = json_decode($body);
-	$producto->id = $id;
-	$dao = new ProductoDAO();
-	$dao->actualizar($producto);
-	echo json_encode(array('mensaje' => 'Producto actualizado satisfactoriamente'));
-});
+ * $app->put('/productos/:id', function($id) use ($app) {
+ * $body = $app->request->getBody();
+ * $producto = json_decode($body);
+ * $producto->id = $id;
+ * $dao = new ProductoDAO();
+ * $dao->actualizar($producto);
+ * echo json_encode(array('mensaje' => 'Producto actualizado satisfactoriamente'));
+ * });
+ *
+ * $app->delete('/productos/:id', function($id) {
+ * $dao = new ProductoDAO();
+ * $dao->eliminar($id);
+ * echo json_encode(array('mensaje' => 'Producto eliminado satisfactoriamente'));
+ * });
+ */
 
-$app->delete('/productos/:id', function($id) {
-	$dao = new ProductoDAO();
-	$dao->eliminar($id);
-	echo json_encode(array('mensaje' => 'Producto eliminado satisfactoriamente'));
-});
-*/
+$app->get ( '/avisos', function () {
+	$dao = new AvisoDAO ();
+	$lista = $dao->listar ();
+	echo json_encode ( $lista );
+} );
 
-$app->get('/avisos', function() {
-	$dao = new AvisoDAO();
-	$lista = $dao->listar();
-	echo json_encode($lista);
-});
+$app->get ( '/avisos/:titulo', function ($titulo) {
+	$dao = new AvisoDAO ();
+	$objeto = $dao->buscarPorTitulo ( $titulo );
+	echo json_encode ( $objeto );
+} );
 
-$app->get('/avisos/:titulo', function($titulo) {
-	$dao = new AvisoDAO();
-	$objeto = $dao->buscarPorTitulo($titulo);
-	echo json_encode($objeto);
-});
-
-$app->post('/avisos', function() use ($app) {
-	$titulo = $app->request()->post('titulo');
-	$fechaInicio = $app->request()->post('fechaInicio');
-	$fechaFin = $app->request()->post('fechaFin');
-
-	$vo = new Aviso();
+$app->post ( '/avisos', function () use ($app) {
+	$titulo = $app->request ()->post ( 'titulo' );
+	$fechaInicio = $app->request ()->post ( 'fechaInicio' );
+	$fechaFin = $app->request ()->post ( 'fechaFin' );
+	
+	$vo = new Aviso ();
 	$vo->titulo = $titulo;
 	$vo->fechaInicio = $fechaInicio;
-	$vo->fechaFin = $fechaFin; 
+	$vo->fechaFin = $fechaFin;
+	
+	$dao = new AvisoDAO ();
+	$dao->insertar ( $vo );
+	echo json_encode ( array (
+			'mensaje' => 'Aviso registrado satisfactoriamente' 
+	) );
+} );
 
-	$dao = new AvisoDAO();
-	$dao->insertar($vo);
-	echo json_encode(array('mensaje' => 'Aviso registrado satisfactoriamente'));
-});
+$app->get ( '/noticias', function () {
+	$dao = new NoticiaDAO ();
+	$lista = $dao->listarRecientes ();
+	echo json_encode ( $lista );
+} );
 
-$app->get('/noticias', function() {
-	$dao = new NoticiaDAO();
-	$lista = $dao->listarRecientes();
-	echo json_encode($lista);
-});
+$app->get ( '/noticias/:idNoticia', function ($idNoticia) {
+	$dao = new NoticiaDAO ();
+	$objeto = $dao->obtener ( $idNoticia );
+	echo json_encode ( $objeto );
+} );
 
-$app->get('/noticias/:idNoticia', function($idNoticia) {
-	$dao = new NoticiaDAO();
-	$objeto = $dao->obtener($idNoticia);
-	echo json_encode($objeto);
-});
-
-$app->post('/mensajes', function() use ($app) {
-	$tipo = $app->request()->post('tipo');
-	$descripcion = $app->request()->post('descripcion');
-	$archivo= $app->request()->post('archivo');
-	$foto = $app->request()->post('foto');
-
-	$vo = new Mensaje();
+$app->post ( '/mensajes', function () use ($app) {
+	$tipo = $app->request ()->post ( 'tipo' );
+	$descripcion = $app->request ()->post ( 'descripcion' );
+	$archivo = $app->request ()->post ( 'archivo' );
+	$foto = $app->request ()->post ( 'foto' );
+	
+	$vo = new Mensaje ();
 	$vo->tipo = $tipo;
 	$vo->descripcion = $descripcion;
 	$vo->archivo = $archivo;
-
-	$dao = new MensajeDAO();
-	$dao->insertar($vo);
 	
-	$data = base64_decode($foto);
-	//file_put_contents('fotos/' . $archivo, $data);
-	file_put_contents('/home/rvilela/public_html/fotos/' . $archivo, $data);
-	echo json_encode(array('mensaje' => 'Mensaje registrado satisfactoriamente'));
-});
+	$dao = new MensajeDAO ();
+	$dao->insertar ( $vo );
+	
+	$data = base64_decode ( $foto );
+	// file_put_contents('fotos/' . $archivo, $data);
+	file_put_contents ( '/home/rvilela/public_html/fotos/' . $archivo, $data );
+	echo json_encode ( array (
+			'mensaje' => 'Mensaje registrado satisfactoriamente' 
+	) );
+} );
 
-$app->post('/dispositivos', function() use ($app) {
-	$usuario = $app->request()->post('usuario');
-	$codigoGCM = $app->request()->post('codigoGCM');
-	$aplicacion = $app->request()->post('aplicacion');
-	$mac = $app->request()->post('mac');
-
-	$vo = new Dispositivo();
+$app->post ( '/dispositivos', function () use ($app) {
+	$usuario = $app->request ()->post ( 'usuario' );
+	$codigoGCM = $app->request ()->post ( 'codigoGCM' );
+	$aplicacion = $app->request ()->post ( 'aplicacion' );
+	$mac = $app->request ()->post ( 'mac' );
+	
+	$vo = new Dispositivo ();
 	$vo->usuario = $usuario;
 	$vo->codigoGCM = $codigoGCM;
 	$vo->aplicacion = $aplicacion;
 	$vo->mac = $mac;
+	
+	$dao = new DispositivoDAO ();
+	$dao->insertar ( $vo );
+	echo json_encode ( array (
+			'estado' => 'CORRECTO' 
+	) );
+} );
 
-	$dao = new DispositivoDAO();
-	$dao->insertar($vo);    
-	echo json_encode(array('estado' => 'CORRECTO'));
-});
-
-$app->run();
+$app->run ();
 
 ?>
